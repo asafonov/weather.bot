dd47bc73a753ff6ef8ddecd4054ce1e2effdc33b
asafonov <me@asafonov.org>
2026-07-02 11:51:28 +0000

    typo fix

--- config.sample.php
+++ config.sample.php
@@ -8,5 +8,5 @@ define('WORKER_LOG_PATH', '/tmp');
 define('MAX_RETRIES', 3);
 define('WEATHER_API_KEY', 'WEATHER_API_KEY');
 define('WEATHER_CACHE_ALIVE_TIME', 1800);
-define('START_MESSAGE', "Hello!\nThis is Weather: Cool and Hot bot. Send me the city you want to know the weather in and I will give you the detailed forecast. Sorry English only accepted.")
-define('PLACE_ERROR_MESSAGE', "Sorry, I didn't get it. Please write a city in English.")
+define('START_MESSAGE', "Hello!\nThis is Weather: Cool and Hot bot. Send me the city you want to know the weather in and I will give you the detailed forecast. Sorry English only accepted.");
+define('PLACE_ERROR_MESSAGE', "Sorry, I didn't get it. Please write a city in English.");
