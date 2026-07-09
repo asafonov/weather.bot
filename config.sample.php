b3863a78cdab273320d9a8849bf30bcc6060c596
asafonov <me@asafonov.org>
2026-07-09 07:39:29 +0000

    minor start fix

--- config.sample.php
+++ config.sample.php
@@ -8,6 +8,6 @@ define('WORKER_LOG_PATH', '/var/log/weather.bot');
 define('MAX_RETRIES', 3);
 define('WEATHER_API_KEY', 'WEATHER_API_KEY');
 define('WEATHER_CACHE_ALIVE_TIME', 1800);
-define('START_MESSAGE', "Hello!\nThis is Weather: Cool and Hot bot. Send me the city you want to know the weather in and I will give you the detailed forecast. Sorry English only accepted.");
+define('START_MESSAGE', "Hello!\nThis is Weather: Cool and Hot bot. Send me the city you want to know the weather in and I will give you the detailed forecast. I also will do my best to send you daily weather updates in this city.");
 define('PLACE_ERROR_MESSAGE', "Sorry, I didn't get it. Please write a city in English.");
 define('SCHEDULE_UPDATE_HOUR', 9);
