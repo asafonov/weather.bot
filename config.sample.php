63dabefbc5221506046fba6498e11c6e9c0b8d88
asafonov <me@asafonov.org>
2026-07-24 07:56:27 +0000

    fix config

--- config.sample.php
+++ config.sample.php
@@ -1,11 +1,11 @@
 <?php
 
 define('TOKEN', 'TELEGRAM_API_TOKEN');
+define('BOT_NAME', 'telegram.bot');
 define('PHP_BIN', '/usr/bin/php');
-define('WORKER_PATH', '/var/www/html/weather.bot');
-define('WORKER_CACHE_PATH', '/var/cache/weather.bot');
-define('WORKER_LOG_PATH', '/var/log/weather.bot');
-define('MAX_RETRIES', 3);
+define('WORKER_PATH', '/var/www/html/' . BOT_NAME);
+define('WORKER_CACHE_PATH', '/var/cache/' . BOT_NAME);
+define('WORKER_LOG_PATH', '/var/log/' . BOT_NAME);
 define('WEATHER_API_KEY', 'WEATHER_API_KEY');
 define('WEATHER_CACHE_ALIVE_TIME', 1800);
 define('START_MESSAGE', "Hello!\nThis is Weather: Cool and Hot bot. Send me the city you want to know the weather in and I will give you the detailed forecast. I also will do my best to send you daily weather updates in this city.");
